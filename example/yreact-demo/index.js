f2de0b754f812a9c86de535a6cd9faf2a12901df
leeson <spli03@mail.yst.com.cn>
2020-11-11 23:15:27 +0800

    feat: add commit phases

--- example/yreact-demo/index.js
+++ example/yreact-demo/index.js
@@ -1,8 +1,32 @@
 /** @jsx Yreact.createElement */
+let dom = []
+for(let i = 0; i < 100; i++) {
+  dom.push({
+    type: 'span',
+    props: {
+      children: [{
+        type: 'TEXT_ELEMENT',
+        props: {
+          nodeValue: i,
+          children: []
+        }
+      }]
+    }
+  })
+}
+
 const App = (
   <div>
     <h1>yreact-text</h1>
     <h2>yreact-demo</h2>
+    {
+    {
+      type: 'div',
+      props: {
+        children: dom
+      }
+    }
+    }
   </div>
 )
 const container = document.getElementById('root')
